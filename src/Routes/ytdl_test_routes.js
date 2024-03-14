06f435f773ab5ec3b38c0a31577b6a8206bcf8d5
Dhana <gautham523sai@gmail.com>
2024-03-14 18:44:44 +0530

    working on ytdl

--- src/Routes/ytdl_test_routes.js
+++ src/Routes/ytdl_test_routes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const Joi = require("joi");
+const ytdl = require("ytdl-core");
 
 const YTDLRoutes = express.Router();
 
-YTDLRoutes.post("/ytdl/test", (req, res) => {
+YTDLRoutes.post("/ytdl/test", async (req, res) => {
   const schema = Joi.object({
     url: Joi.string().optional().allow(null, ""),
   });
@@ -12,6 +13,8 @@ YTDLRoutes.post("/ytdl/test", (req, res) => {
     if (error) {
       console.log(error);
     }
+    const videoID = await ytdl.getURLVideoID(body.url);
+    console.log(await ytdl.getInfo(videoID));
     res.end();
   } catch (error) {
     console.log(error);
